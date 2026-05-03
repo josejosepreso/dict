3c347498be43918109704cb58df6af04eca096e6
josejosepreso <josehihat@gmail.com>
2026-05-03 16:35:31 -0600

    add imperative algo

--- main.cpp
+++ main.cpp
@@ -1,60 +1,106 @@
+#include <algorithm>
 #include <cstring>
+#include <fstream>
 #include <iostream>
-#include <map>
+#include <stdexcept>
+#include <string>
+#include <thread>
+#include <vector>
 
 #define DICTIONARY_FILE_PATH "db.txt"
+#define DB_CHUNK_SIZE (64 * 1024)
+#define THREAD_COUNT 4
+int line_number;
 
-int main(int argc, char *argv[])
-{
-  char *target = argv[1];
+int levenstein_recursive(const std::string &str1, const std::string &str2,
+                         int str1length, int str2length) {
+  if (str1length == 0)
+    return str2length;
 
-  if (target == NULL) {
-    std::cout << "Usage: "
-              << "\tdict <word>" << std::endl;
+  if (str2length == 0)
+    return str1length;
+
+  if (str1[str1length - 1] == str2[str2length - 1])
+    return levenstein_recursive(str1, str2, str1length - 1, str2length - 1);
+
+  return 1 + std::min(levenstein_recursive(str1, str2, str1length, str2length - 1),
+                  std::min(levenstein_recursive(str1, str2, str1length - 1, str2length),
+                           levenstein_recursive(str1, str2, str1length - 1, str2length - 1)));
+}
+
+// TODO: make this multithreaded
+int levenstein(const std::string &str1, const std::string &str2) {
+  int m = str1.length();
+  int n = str2.length();
+
+  std::vector<std::vector<int>> dp(m + 1, std::vector<int>(n + 1, 0));
+
+  for (int i = 0; i <= m; i++)
+    dp[i][0] = i;
+
+  for (int j = 0; j <= n; j++)
+    dp[0][j] = j;
+
+  for (int i = 1; i <= m; i++)
+    for (int j = 1; j <= n; j++) {
+      if (str1[i - 1] == str2[j - 1])
+        dp[i][j] = dp[i - 1][j - 1];
+      else
+        dp[i][j] = 1 + std::min(dp[i][j - 1], std::min(dp[i - 1][j], dp[i - 1][j - 1]));
+    }
+
+  return dp[m][n];
+}
+
+typedef struct {
+  std::string word;
+  int distance;
+} word_distance_t;
+
+std::vector<std::string> get_db_lines(const char c = '\0') {
+  std::ifstream istr(DICTIONARY_FILE_PATH);
+
+  if (!istr.is_open())
+    throw std::runtime_error("couldn't open db file.");
+
+  std::vector<std::string> lines;
+  std::string line;
+
+  while (std::getline(istr, line))
+    if (c == line[0])
+      lines.push_back(line);
+
+  istr.close();
+
+  return lines;
+}
+
+int main(int args, char **vargs) {
+  if (args < 2)
     return EXIT_FAILURE;
-  }
 
-  FILE *f = fopen(DICTIONARY_FILE_PATH, "r");
+  const std::string target(vargs[1]);
 
-  if (f == NULL) {
-    std::cout << "Couldn't open file " << DICTIONARY_FILE_PATH << std::endl;
-    return EXIT_FAILURE;
-  }
+  std::vector<std::string> lines(get_db_lines(vargs[1][0]));
 
-  char l[256];
-  char *curr;
-  char *curr_key;
-  char *curr_val;
+  std::vector<word_distance_t> out(lines.size());
 
-  std::map<std::string, std::string> db;
+  auto get_translation = [target](std::string &word) -> word_distance_t {
+    std::string copy = word;
+    std::string word_to_compare = strtok(word.data(), ":");
+    return word_distance_t{copy, levenstein(target, word_to_compare)};
+  };
 
-  while (fgets(l, sizeof l, f) != NULL) {
-    printf("%s", l);
-    curr_key = strtok(l, ":");
-    curr_val = strtok(NULL, "");
+  std::transform(lines.begin(), lines.end(), out.begin(), get_translation);
 
-    if (curr_key == NULL || curr_val == NULL)
-      break;
+  auto get_closest_distance = [](const word_distance_t &a, const word_distance_t &b) -> bool {
+    return a.distance < b.distance;
+  };
 
-    db.insert(std::pair{ std::string(curr_key), std::string(curr_val) });
+  auto closest = *std::min_element(out.begin(), out.end(), get_closest_distance);
 
-    // if (strcmp(target, curr) != 0)
-    //   continue;
-
-    // std::cout << target << ":" << std::endl;
-
-    // curr = strtok(NULL, ",");
-
-    // while (curr != NULL) {
-    //   std::cout << "\t" << curr;
-    //   curr = strtok(NULL, ",");
-
-    //   if (curr != NULL)
-    //     std::cout << std::endl;
-    // }
-
-    // break;
-  }
+  std::cout << "Target: " << target << std::endl;
+  std::cout << "Result: " << closest.word << " (distance: " << closest.distance << ")" << std::endl;
 
   return 0;
 }
